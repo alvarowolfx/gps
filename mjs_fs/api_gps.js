de43a992dcb8054842ba7d98745f75e6c7b2599d
Alvaro Viebrantz <alvarowolfx@gmail.com>
2017-12-13 11:36:29 -0300

    [fix] Type on api_gps

--- mjs_fs/api_gps.js
+++ mjs_fs/api_gps.js
@@ -2,21 +2,20 @@
 // [mgos_gps.h]
 
 let GPS = {
-    _get_location: ffi('char * mgos_get_location()')
-  
-    // ## **`GPS.getLocation()`**
-    // Get last location obtained
-    getLocation: function() {
-        let latlon = GPS._get_location();
-        let jsonParsed = JSON.parse(latlon);
-        if (
-            jsonParsed.sp === 'nan' ||
-            jsonParsed.lon === 'nan' ||
-            jsonParsed.lat === 'nan'
-        ) {
-            return false;
-        }      
-        return jsonParsed;
+  _get_location: ffi('char * mgos_get_location()'),
+
+  // ## **`GPS.getLocation()`**
+  // Get last location obtained
+  getLocation: function() {
+    let latlon = GPS._get_location();
+    let jsonParsed = JSON.parse(latlon);
+    if (
+      jsonParsed.sp === 'nan' ||
+      jsonParsed.lon === 'nan' ||
+      jsonParsed.lat === 'nan'
+    ) {
+      return false;
     }
-  };
-  
+    return jsonParsed;
+  }
+};
